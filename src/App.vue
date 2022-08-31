e2878ece78b8775ab3d5353c958f477a50e423c8
WilhamsGomes <wilhamsgoomes@gmail.com>
2022-08-31 17:14:22 -0300

    Projeto com vuetify

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app id="inspire">
+   
+    <Header @handeDrawer="drawer = !drawer" />
+    <Sidebar :drawer="drawer"/>
+
+    <v-main>
+      <v-container class="py-8 px-6" fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Sidebar from "./components/Sidebar.vue";
+
+  export default {
+    data: () => ({
+        cards: ["Today", "Yesterday"],
+        drawer: true,
+    }),
+    components: { 
+      Header, 
+      Sidebar 
+    }
+}
+</script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,54 @@
+<template>
+
+    <v-navigation-drawer v-model="drawer" app>
+        <v-img src="https://preview.pixlr.com/images/800wm/1439/2/1439104804.jpg" class="pa-4">
+            <div class="text-center mt-4">
+                <v-avatar class="mb-4" color="grey darken-1" size="64">
+                    <v-img aspect-ratio="30" src="https://randomuser.me/api/portraits/women/81.jpg"/>
+                </v-avatar>
+                <h2 class="white--text">Vuetify Admin Panel</h2>
+            </div>
+        </v-img>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          v-for="[icon, text] in links"
+          :key="icon"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  
+</template>
+
+<script>
+    export default {
+        props: ['drawer'],
+        name:"Sidebar",
+        data(){
+            return{
+                links: [
+                    ["mdi-microsoft-windows", "Dashboard"],
+                    ["mdi-account", "Profile"],
+                    ["mdi-clipboard-list-outline", "Product"],
+                    ["mdi-clipboard-list", "Order"],
+                    ["mdi-cog-outline", "System Settings"],
+                ],
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
